390f1a07787c4ecea1de073dd5e766ebd2ac6405
yangchao <1162485779@qq.com>
2021-11-05 10:46:36 +0800

    qnx test

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-cmake -S"./" -B"./res/proj" -DRES_DIR="./res" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=Debug && make -C ./res/proj
+cmake -S"./" -B"./res/proj" -DRES_DIR="./res" -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=./qnx2.cmake && make -C ./res/proj
